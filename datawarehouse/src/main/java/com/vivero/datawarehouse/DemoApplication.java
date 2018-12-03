29dac93c521bbb01b0dfd0c7667a5e6aff00f252
Andrey Blandón Mora <andreybm31@disroot.org>
2018-12-02 22:56:47 -0600

    first commit

---
+++ datawarehouse/src/main/java/com/vivero/datawarehouse/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.vivero.datawarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
